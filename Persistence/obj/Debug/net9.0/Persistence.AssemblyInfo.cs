22a952290fbadf94a7aa77e588583078c9160b85
peterdinis <petter1@pobox.sk>
2025-11-02 19:22:56 +0100

    fix: Fixing problematic migrations

--- Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+120af3e8b8145da157eca18e21c28b7ac117a5e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58e24ada945e56ffac7d0606477ec5ffa6e0fb36")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
