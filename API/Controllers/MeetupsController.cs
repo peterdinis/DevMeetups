3d978ad5d464d6ca3841385462d90371ee04e91a
peterdinis <petter1@pobox.sk>
2025-11-08 15:16:46 +0100

    feat: Formatting files

--- API/Controllers/MeetupsController.cs
+++ API/Controllers/MeetupsController.cs
@@ -10,7 +10,7 @@ public class MeetupsController : BaseApiController
     [HttpGet]
     public async Task<ActionResult<List<Meetup>>> GetMeetups()
     {
-         return await Mediator.Send(new GetMeetupList.Query());
+        return await Mediator.Send(new GetMeetupList.Query());
     }
 
     [HttpGet("{id}")]
@@ -18,17 +18,17 @@ public class MeetupsController : BaseApiController
     {
         return await Mediator.Send(new GetMeetupDetails.Query { Id = id });
     }
-    
+
     [HttpPost]
     public async Task<ActionResult<string>> CreateMeetup(Meetup meetup)
     {
-        return await Mediator.Send(new CreateMeetup.Command{Meetup = meetup});
+        return await Mediator.Send(new CreateMeetup.Command { Meetup = meetup });
     }
 
     [HttpPut]
     public async Task<ActionResult> EditMeetup(Meetup meetup)
     {
-        await Mediator.Send(new EditMeetup.Command{Meetup = meetup});
+        await Mediator.Send(new EditMeetup.Command { Meetup = meetup });
 
         return NoContent();
     }
@@ -36,7 +36,7 @@ public class MeetupsController : BaseApiController
     [HttpDelete("{id}")]
     public async Task<ActionResult> DeleteMeetup(string id)
     {
-        await Mediator.Send(new DeleteMeetup.Command{Id = id});
+        await Mediator.Send(new DeleteMeetup.Command { Id = id });
 
         return Ok();
     }
