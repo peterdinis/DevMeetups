d8557ec77a47148dea4a43bc0646a4a9ae862e1b
peterdinis <petter1@pobox.sk>
2025-11-09 16:40:48 +0100

    feat: Start working on frontend WIP

--- Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3885ee66a58d7752d4ec4be4dd977e47c96fc1b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9405f6ebbc79d021970da607522f8e7a76e7153")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
