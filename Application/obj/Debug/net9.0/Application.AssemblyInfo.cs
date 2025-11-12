c87ac73443d590ec098615a03cae186c2430a084
peterdinis <petter1@pobox.sk>
2025-11-12 19:42:31 +0100

    FOO

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9405f6ebbc79d021970da607522f8e7a76e7153")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a114198f6c2b881e8618c67a9d03cc4e9ca314a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
