ea319aa45afd091ebd5d344b00e29614d8ada3f9
peterdinis <petter1@pobox.sk>
2025-11-13 19:14:16 +0100

    feat: Working on frontend WIP

--- frontend/routes/router.tsx
+++
@@ -1,6 +0,0 @@
-import { createRouter } from '@tanstack/react-router'
-import { rootRoute } from './__root'
-
-export const router = createRouter({
-  routeTree: rootRoute,
-})
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,5 @@
 import { RouterProvider } from "@tanstack/react-router";
 import type { FC } from "react";
-import {router} from "../routes/router"
-
 const App: FC = () => {
   return <RouterProvider router={router} />
 }
