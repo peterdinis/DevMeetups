145750ecb7ad6a48f9ca04193e72512928940b22
peterdinis <petter1@pobox.sk>
2025-11-16 20:28:58 +0100

    feat: Start working on frontend WIP

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { createRoute } from '@tanstack/react-router'
 import { rootRoute } from './__root'
+import { Button } from '@chakra-ui/react'
 
 function Index() {
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+      <Button>TEST ME</Button>
     </div>
   )
 }
