f11dc2be9d48b860c417fef9970cda0307c98f72
peterdinis <petter1@pobox.sk>
2025-11-22 16:46:17 +0100

    feat: Migrations for identity are DONE

--- Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+594f98099cdc3fcbcc128edfebea99aeb4e0402b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd972328ed4eaada9b7ff9283d353a5ed4520da8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
