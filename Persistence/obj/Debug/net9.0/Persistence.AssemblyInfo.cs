c089ed2e62bdec997fdab9311cfbb87a9e9cfd75
peterdinis <petter1@pobox.sk>
2025-11-29 19:01:11 +0100

    feat: Let start working on frontend Auth WIP

--- Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3beffb859ec23ad0c44604b9c9403c1fd64268f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+479e2a0d7f6ca6394f5898a6a223b3abc2364f29")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
