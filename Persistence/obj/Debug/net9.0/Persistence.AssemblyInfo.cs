410a9e027e4b9161e1c7872d9176645b76487eae
peterdinis <petter1@pobox.sk>
2025-12-06 16:54:26 +0100

    feat: Frontend auth DONE

--- Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c621cca766c3ee7bc1759e0f87db856806e1c1f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c4c68411cd74a505e29d7f5d07def46ed672c0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
